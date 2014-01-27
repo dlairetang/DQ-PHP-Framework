7451e3f56f75d38fd9f824a00274a906988b3cf7
dlairetang <dlairetang@gmail.com>
2014-01-28 03:20:29 +0800

    Revert "DQ201312231333"
    This reverts commit f1e9e2c37ed15e0334dddb1c79544202ecfaf628.

--- System/sys.php
+++ System/sys.php
@@ -41,10 +41,6 @@ class M extends D
 	{
 	require("./System/addons/".$addons.".addons.php");
 	}
-	public function Sysinstances($class)
-	{
-	return new $class.'Addons';
-	}
     public function assign($what,$target)
     {
         $this->tpl->assign($what,$target);
